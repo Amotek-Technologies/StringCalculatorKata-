82097a046f409112eb1f13b73499d05814ebd842
tatev-avet <tatevik@swaps.app>
2022-09-19 15:20:25 +0400

    first step for working test

---
+++ src/StringCalculator.test.ts
@@ -0,0 +1,16 @@
+const add = (input:string) => {
+    return 0
+}
+
+describe('StringCalculator', () => {
+  it("empty input", () => {
+    // Arrange
+  
+
+    // Act
+ 
+
+    // Assert
+    expect(add("")).toBe(0)
+  })
+})
